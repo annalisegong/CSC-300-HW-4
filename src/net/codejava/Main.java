a8471eba8ab2b3786fdaec7577a130bedfff71f8
annalisegong <annalise.gong@cuw.edu>
2022-09-15 13:12:40 -0500

    test

--- src/net/codejava/Main.java
+++ src/net/codejava/Main.java
@@ -2,23 +2,16 @@ package net.codejava;
 
 public class Main 
 {
-
 	public static void main(String[] args) 
 	{
 		String s = "hey";
 		
 		for(int i = 0; i <= s.length()-1; i++)
 		{
-			int count = 0;
-			char a = s.charAt(count);
-			char b = s.charAt(i);
-			if(a.equals(b))
+			if(s(i).equals(s(i+1)))
 			{
 				System.out.println("String: " + s + ", does not have all unique characters");
 			}
-			count++;
 		}
-
 	}
-
 }
