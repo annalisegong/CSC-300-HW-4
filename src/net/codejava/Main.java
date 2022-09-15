65928c957bbeeeff00377f274780c1d7c8b69340
annalisegong <annalise.gong@cuw.edu>
2022-09-15 15:05:41 -0500

    completed hw
    used primitive operators and charAt to compare string contents

--- src/net/codejava/Main.java
+++ src/net/codejava/Main.java
@@ -4,28 +4,29 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		String s = "hello";
+		String s = "abc";
 		if(uniqueTest(s) == false)
 		{
-			System.out.println("' " + s + "' does not have all unique characters");
+			System.out.println("'" + s + "' does not have all unique characters");
 		}
 		else
 		{
-			System.out.println("' " + s + "' has all unique characters");
+			System.out.println("'" + s + "' has all unique characters");
 		}
 	}
 	
 	public static boolean uniqueTest(String s)
 	{
-		for(int i = 0; i <= s.length(); i++)
+		for(int i = 0; i < s.length()-1; i++)
 		{
-			for(int j = 0; j <= s.length(); j++)
+			for(int j = 0; j < s.length() - i - 1; j++)
 			{
-				if(s.charAt(i).equals(s.charAt(j)))
+				if(s.charAt(i) == s.charAt(i+1))
 				{
 					return false;
 				}
 			}
 		}
+		return true;
 	}
 }
