49c3a64a549a813cf83459d087d632e73f91992c
annalisegong <annalise.gong@cuw.edu>
2022-09-15 15:11:21 -0500

    perfected
    placed output lines within boolean method

--- src/net/codejava/Main.java
+++ src/net/codejava/Main.java
@@ -4,15 +4,11 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		String s = "abc";
-		if(uniqueTest(s) == false)
-		{
-			System.out.println("'" + s + "' does not have all unique characters");
-		}
-		else
-		{
-			System.out.println("'" + s + "' has all unique characters");
-		}
+		String s1 = "hello";
+		String s2 = "bye";
+		
+		uniqueTest(s1);
+		uniqueTest(s2);
 	}
 	
 	public static boolean uniqueTest(String s)
@@ -23,10 +19,12 @@ public class Main
 			{
 				if(s.charAt(i) == s.charAt(i+1))
 				{
+					System.out.println("'" + s + "' does not have all unique characters");
 					return false;
 				}
 			}
 		}
+		System.out.println("'" + s + "' has all unique characters");
 		return true;
 	}
 }
