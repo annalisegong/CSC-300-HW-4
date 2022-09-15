931da214277106bea0ecddeb8107aa75aa13d8e2
annalisegong <annalise.gong@cuw.edu>
2022-09-15 14:13:36 -0500

    added main method
    still need to fix if statement

--- src/net/codejava/Main.java
+++ src/net/codejava/Main.java
@@ -4,15 +4,26 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		String s = "hey";
-		
+		String s = "hello";
+		if(uniqueTest(s) == false)
+		{
+			System.out.println("' " + s + "' does not have all unique characters");
+		}
+		else
+		{
+			System.out.println("' " + s + "' has all unique characters");
+		}
+	}
+	
+	public static boolean uniqueTest(String s)
+	{
 		for(int i = 0; i <= s.length(); i++)
 		{
-			for(int j = 0; j < s.length(); j++)
+			for(int j = 0; j <= s.length(); j++)
 			{
 				if(s.charAt(i).equals(s.charAt(j)))
 				{
-					System.out.println("String: " + s + ", does not have all unique characters");
+					return false;
 				}
 			}
 		}
